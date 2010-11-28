016999894bad2763d2e3e57b5d00a5c9f5644bd9
Here I. Come <me.detected@gmail.com>
2010-11-28 20:18:49 +0300

    useless lines

--- rg-flac.sh
+++ rg-flac.sh
@@ -3,6 +3,4 @@
 # finds flac music in your collection without replaygain data and adds it to
 # them
 
-# ivvmm Вск Ноя 21 05:17:57 MSK 2010
-
 find . -type f -name "*.flac" | while read file; do dirname "${file}" ; done | sort | uniq | while read dir; do cd "${dir}" && { pwd; { for i in *.flac ; do metaflac --export-tags=- "$i" | grep -q REPLAYGAIN || echo found_no_replay_gain_tracks ; done; } | grep found_no_replay_gain_tracks && metaflac --add-replay-gain *.flac ; cd -; } ; done
